302abaf3212c933f924b87439dcd9fec76d0bc43
Vitor Gabriel Blasques Victor <vitorvictorblasq@gmail.com>
2025-10-22 15:25:50 -0300

    update: banner vertical pegando notcias principais na home

--- src/app/(public-routes)/page.tsx
+++ src/app/(public-routes)/page.tsx
@@ -40,54 +40,53 @@ export default async function HomePage() {
           </div>
         </div>
 
-        {/* Notícias Principais */}
-        {principais_noticias && principais_noticias.length > 0 && (
-          <div className="space-y-4">
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-12 lg:grid-rows-3 gap-4 lg:max-h-[500px]">
-              {principais_noticias.map((news: any, index: number) => {
-                if (index === 0)
-                  return (
-                    <NewsMain
-                      key={news.id}
-                      title={news.title}
-                      description={news.sub_title}
-                      badge={news.badge}
-                      time={news.created_at}
-                      image={`${process.env.NEXT_PUBLIC_IMAGE_URL}${news.image_url}`}
-                      className="sm:col-span-2 lg:col-span-7 lg:row-span-3"
-                      slug={news.slug}
-                    />
-                  );
-                return (
-                  <NewsSecondary
-                    key={news.id}
-                    title={news.title}
-                    description={news.sub_title}
-                    badge={news.badge}
-                    time={news.created_at}
-                    image={`${process.env.NEXT_PUBLIC_IMAGE_URL}${news.image_url}`}
-                    className="sm:col-span-1 lg:col-span-5"
-                    slug={news.slug}
-                  />
-                );
-              })}
-            </div>
-
-            <div className="flex justify-center">
-              <SeeMoreBtn
-                path="/noticias"
-                label="Ver todas as notícias"
-                className="h-10 border border-primary/20 bg-background shadow-xs hover:bg-accent hover:text-accent-foreground dark:bg-input/30 dark:border-input dark:hover:bg-input/50"
-                variant="outline"
-              />
-            </div>
-          </div>
-        )}
-
         {/* Grid principal com conteúdo e banner lateral */}
         <div className="grid lg:grid-cols-5 gap-6">
           {/* Conteúdo principal - 4 colunas */}
           <div className="lg:col-span-4 space-y-2 lg:space-y-8">
+            {/* Notícias Principais */}
+            {principais_noticias && principais_noticias.length > 0 && (
+              <div className="space-y-4">
+                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-12 lg:grid-rows-3 gap-4 lg:max-h-[500px]">
+                  {principais_noticias.map((news: any, index: number) => {
+                    if (index === 0)
+                      return (
+                        <NewsMain
+                          key={news.id}
+                          title={news.title}
+                          description={news.sub_title}
+                          badge={news.badge}
+                          time={news.created_at}
+                          image={`${process.env.NEXT_PUBLIC_IMAGE_URL}${news.image_url}`}
+                          className="sm:col-span-2 lg:col-span-7 lg:row-span-3"
+                          slug={news.slug}
+                        />
+                      );
+                    return (
+                      <NewsSecondary
+                        key={news.id}
+                        title={news.title}
+                        description={news.sub_title}
+                        badge={news.badge}
+                        time={news.created_at}
+                        image={`${process.env.NEXT_PUBLIC_IMAGE_URL}${news.image_url}`}
+                        className="sm:col-span-1 lg:col-span-5"
+                        slug={news.slug}
+                      />
+                    );
+                  })}
+                </div>
+
+                <div className="flex justify-center">
+                  <SeeMoreBtn
+                    path="/noticias"
+                    label="Ver todas as notícias"
+                    className="h-10 border border-primary/20 bg-background shadow-xs hover:bg-accent hover:text-accent-foreground dark:bg-input/30 dark:border-input dark:hover:bg-input/50"
+                    variant="outline"
+                  />
+                </div>
+              </div>
+            )}
             {/* Banner Home 1 */}
             {banners_home &&
               banners_home["home_1"] &&
